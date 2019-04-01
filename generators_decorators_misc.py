94c8d8487bd087914d70061db5c2190b1387575b
Ammar Abdilghanie <ama66@cornell.edu>
2019-04-01 01:11:20 -0500

    Add files via upload

--- generators_decorators_misc.py
+++ generators_decorators_misc.py
@@ -229,6 +229,89 @@ hello_func()
 
 #####################################
 
+print("\n")
+print("*"*40)
+def decorator_function(original_function):
+
+	def wrapper_function(*args,**kwargs):
+		## Added functionality
+		print("Wrapper Added this before running {}".format(original_function.__name__))
+		return original_function(*args,**kwargs)
+	return wrapper_function
+
+
+
+@decorator_function
+def display():
+	print("Display function ran! ")
+
+
+@decorator_function
+def display_info(name,age):
+	print("Display info ran with arguments({},{})".format(name,age))
+
+display_info("Ammar", 41)
+
+
+
+
+#decorated_display=decorator_function(display)
+
+#decorated_display()
+
+display()
+print("*"*40)
+
+
+################ Decorator Class
+
+# class decorator_class(object):
+
+#     def __init__(self, original_function):
+#         self.original_function = original_function
+
+#     def __call__(self, *args, **kwargs):
+#         print('call method before {}'.format(self.original_function.__name__))
+#         self.original_function(*args, **kwargs)
+
+
+# @decorator_class
+# def display_me(name,age):
+# 	print("Display me ran with arguments({},{})".format(name,age))
+
+# display_me("Ammar", 41)
+
+
+############# Practical Examples ############ 
+# Practical Examples
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
+import time
+
+
 
 
 
