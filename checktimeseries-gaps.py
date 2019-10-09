bb00a82aa54b100866517968a37b40746575ac12
Ammar Abdilghanie <ama66@cornell.edu>
2019-10-09 15:17:32 -0500

    Create checktimeseries-gaps.py

---
+++ checktimeseries-gaps.py
@@ -0,0 +1,12 @@
+#This is the number of rows for full dataset 1+int((df.index.max() - df.index.min()).total_seconds()/60)
+ 
+#Which you can compare to len(df)
+ 
+#(2) 
+#This is the full set of timestamps from start to end of a dataframe
+ 
+#timeindexes= pd.DatetimeIndex(start=dfw.index.min(), end=dfw.index.max(),freq='min')
+ 
+#if every timestamp in the full dataset is in df or the other way around. The expression below should sum to the length of the dataframe
+ 
+#df.index.isin(timeindexes).sum()
