aefac70125a079f785d0c688f274f0fa38c60b6d
Ammar Abdilghanie <ama66@cornell.edu>
2019-10-23 13:01:46 -0500

    Update rewardprob.py

--- rewardprob.py
+++ rewardprob.py
@@ -98,3 +98,81 @@ topologicalSort(jobs,deps)
 
 ## 1 preq >> dep 2  , 1 preq >> dep 3 , 3 preq >> dep 2 , 4 preq >> dep 2  , 4 preq >> dep 3
 
+
+#################################
+# Solution 2 
+###############################
+
+
+# O(j + d) time | O(j + d) space
+def topologicalSort(jobs, deps):
+    jobGraph = createJobGraph(jobs, deps)
+    return getOrderedJobs(jobGraph)
+
+def createJobGraph(jobs, deps):
+    graph = JobGraph(jobs)
+    for job, dep in deps:
+        graph.addDep(job, dep)
+    return graph
+
+def getOrderedJobs(graph):
+    orderedJobs = []
+    nodesWithNoPrereqs = list(filter(lambda node: node.numOfPrereqs == 0, graph.nodes))
+    ## build a list with no prereqs
+    ## iterate/traverse graphs until this list is []
+    while len(nodesWithNoPrereqs):
+        node = nodesWithNoPrereqs.pop()
+        orderedJobs.append(node.job)
+        ## update dependecy prereq count after removing this node from the graph! 
+        removeDeps(node, nodesWithNoPrereqs)
+    graphHasEdges = any(node.numOfPrereqs for node in graph.nodes)
+    return [] if graphHasEdges else orderedJobs
+
+def removeDeps(node, nodesWithNoPrereqs):
+    ## iterate over the dependencies of the node that does not have prereqs
+    ## update their prereq count by -1. if one or more end up with 0 prereqs add it to the nodes with no prereqs list!
+    while len(node.deps):
+        dep = node.deps.pop()
+        dep.numOfPrereqs -= 1
+        if dep.numOfPrereqs == 0:
+            nodesWithNoPrereqs.append(dep)
+
+class JobGraph:
+    def __init__(self, jobs):
+        self.nodes = []
+        self.graph = {}
+        for job in jobs:
+            self.addNode(job)
+
+    def addDep(self, job, dep):
+        jobNode = self.getNode(job)
+        depNode = self.getNode(dep)
+        jobNode.deps.append(depNode)
+        ## for a given jobnode every time you append a dependency need to increase numofprereqs of the
+        ## dependency by 1 
+        depNode.numOfPrereqs += 1
+
+    def addNode(self, job):
+        self.graph[job] = JobNode(job)
+        self.nodes.append(self.graph[job])
+
+    def getNode(self, job):
+        if job not in self.graph:
+            self.addNode(job)
+        return self.graph[job]
+
+class JobNode:
+    def __init__(self, job):
+        self.job = job
+        self.deps = []  ## keep track of dependencies for a given jobnode (i.e. those nodes that need to wait for node to run!)
+        self.numOfPrereqs = 0 
+        
+########
+topologicalSort(jobs,deps)
+## output [4, 1, 3, 2]
+x_l=[0,1,2,5,5,9,0,4,2,0]
+list(filter(lambda elem: elem == 0, x_l))
+## Answer [0, 0, 0]
+
+        
+        
