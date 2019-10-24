80922b8a8bae950f9ce945f3bf471e86ed590c9a
Ammar Abdilghanie <ama66@cornell.edu>
2019-10-24 11:10:19 -0500

    Update rewardprob.py

--- rewardprob.py
+++ rewardprob.py
@@ -1,1 +1,17 @@
 
+def getNeighbors(i, j, board):
+    
+    kernel=[[-1,-1],[-1,0],[-1,1],
+            [0,-1],[0,1],
+            [1,-1],[1,0],[1,1]
+           ]
+    
+    neighbors = []
+    
+    for k in kernel:
+        index_i=i+k[0]
+        index_j=j+k[1]
+        if index_i >=0 and index_i <= len(board)-1 and index_j >=0 and index_j <= len(board[0])-1:
+            neighbors.append([index_i,index_j])
+            
+    return neighbors
