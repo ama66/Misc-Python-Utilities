5191888261401a4ffc7e3c9708c404b0c158ae6b
Ammar Abdilghanie <ama66@cornell.edu>
2019-10-30 12:41:38 -0500

    Update rewardprob.py

--- rewardprob.py
+++ rewardprob.py
@@ -114,5 +114,93 @@ meetingDuration = 45
 #expected = [['15:00', '16:00']]
 calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration)
 
+##########
+
+
+# Merge Sort
+
+# Write a function that takes in an array of integers and returns a sorted version of that array. Use the Merge Sort algorithm to sort the array.
+
+# Sample input: [8, 5, 2, 9, 5, 6, 3]
+# Sample output: [2, 3, 5, 5, 6, 8, 9]
+
+
+#Sol1
+# Copyright © 2019 AlgoExpert, LLC. All rights reserved.
+
+# Best: O(nlog(n)) time | O(nlog(n)) space
+# Average: O(nlog(n)) time | O(nlog(n)) space
+# Worst: O(nlog(n)) time | O(nlog(n)) space
+def mergeSort(array):
+    if len(array) == 1:
+        return array
+    middleIdx = len(array) // 2
+    leftHalf = array[:middleIdx]
+    rightHalf = array[middleIdx:]
+    return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+
+def mergeSortedArrays(leftHalf, rightHalf):
+    sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+    k = i = j = 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] <= rightHalf[j]:
+            sortedArray[k] = leftHalf[i]
+            i += 1
+        else:
+            sortedArray[k] = rightHalf[j]
+            j += 1
+        k += 1
+    while i < len(leftHalf):
+        sortedArray[k] = leftHalf[i]
+        i += 1
+        k += 1
+    while j < len(rightHalf):
+        sortedArray[k] = rightHalf[j]
+        j += 1
+        k += 1
+    return sortedArray
+
+
+# Sol2 
+# Copyright © 2019 AlgoExpert, LLC. All rights reserved.
+
+# Best: O(nlog(n)) time | O(n) space
+# Average: O(nlog(n)) time | O(n) space
+# Worst: O(nlog(n)) time | O(n) space
+def mergeSort(array):
+    if len(array) <= 1:
+        return array
+    auxiliaryArray = array[:]
+    mergeSortHelper(array, 0, len(array) - 1, auxiliaryArray)
+    return array
+
+def mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray):
+    if startIdx == endIdx:
+        return
+    middleIdx = (startIdx + endIdx) // 2
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray)
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray)
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray)
+
+def doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray):
+    k = startIdx
+    i = startIdx
+    j = middleIdx + 1
+    while i <= middleIdx and j <= endIdx:
+        if auxiliaryArray[i] <= auxiliaryArray[j]:
+            mainArray[k] = auxiliaryArray[i]
+            i += 1
+        else:
+            mainArray[k] = auxiliaryArray[j]
+            j += 1
+        k += 1
+    while i <= middleIdx:
+        mainArray[k] = auxiliaryArray[i]
+        i += 1
+        k += 1
+    while j <= endIdx:
+        mainArray[k] = auxiliaryArray[j]
+        j += 1
+        k += 1
 
 
