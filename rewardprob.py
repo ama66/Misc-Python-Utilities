34ec8dc57b776256d80f337eda810c767cc70951
Ammar Abdilghanie <ama66@cornell.edu>
2019-11-24 23:07:13 -0600

    Update rewardprob.py

--- rewardprob.py
+++ rewardprob.py
@@ -22,14 +22,41 @@
 
 # In[90]:
 
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {'2': ['a', 'b', 'c'],
+             '3': ['d', 'e', 'f'],
+             '4': ['g', 'h', 'i'],
+             '5': ['j', 'k', 'l'],
+             '6': ['m', 'n', 'o'],
+             '7': ['p', 'q', 'r', 's'],
+             '8': ['t', 'u', 'v'],
+             '9': ['w', 'x', 'y', 'z']}
+        if digits=="":
+            return []
+            
+        results=[]
+        ## side effect method (will lead to results appended to the results global list) 
+        self.letcomb_help(results,digits,"",phone)
+        return results
+    
+    def letcomb_help(self, results,digits,cur_st,phone):
+        if digits=="":
+            results.append(cur_st)
+            return 
+        
+        for char in phone[digits[0]]:
+            self.letcomb_help(results,digits[1:],cur_st+char,phone)
+            
 # Session 2
 # Medium https://leetcode.com/problems/validate-binary-search-tree/
 ## Definition for a binary tree node.
 ## time complexity O(N) because you need to traverse every single node
 ## space complexity O(log(N)) for a balanced tree otherwise O(N) for a linkedlist like tree (one deep branch!)
 ## and this is the worst case scenario 
-
+     
 class TreeNode:
   def __init__(self, x):
     self.val = x
