6074ed9e38651195b32fc06bf1416111e6015c16
Ammar Abdilghanie <ama66@cornell.edu>
2019-12-13 23:19:14 -0600

    Update Amazon-LeetCode.py

--- Amazon-LeetCode.py
+++ Amazon-LeetCode.py
@@ -344,5 +344,18 @@ print(groupAnagramWords(['abc', 'bcd', 'cba', 'cbd', 'efg']))
 # (['abc', 'cba'], ['bcd', 'cbd'], ['efg'])
 
  #########Algop******************
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m <= 0 or n <= 0:
+            return
+        dp = [[0]*n]*m
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
 
     
