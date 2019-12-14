bc0cf8c3dc9282a37a918d1b532298935e14ab06
Ammar Abdilghanie <ama66@cornell.edu>
2019-12-13 21:29:42 -0600

    Update Amazon-LeetCode.py

--- Amazon-LeetCode.py
+++ Amazon-LeetCode.py
@@ -307,7 +307,19 @@ class Solution:
             L.append(heappop(h)[1])
         return L
     
+# https://leetcode.com/problems/group-anagrams/submissions/
+## Group Anagrams
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+       
+        return ans.values()
     
-
+    
 
 
