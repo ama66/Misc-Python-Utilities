77a235d2f0d36b21346293aa7d640be0a6222b07
Ammar Abdilghanie <ama66@cornell.edu>
2019-12-15 22:22:49 -0600

    Update Amazon-LeetCode.py

--- Amazon-LeetCode.py
+++ Amazon-LeetCode.py
@@ -692,4 +692,21 @@ class Solution:
             
         return res
     
-    
+  ## Remove vowels from a string  
+## 
+def rem_vowel(string): 
+    vowels = ('a', 'e', 'i', 'o', 'u')  
+    for x in string.lower(): 
+        if x in vowels: 
+            string = string.replace(x, "") 
+              
+    # Print string without vowels 
+    print(string) 
+  
+# Driver program 
+string = "GeeksforGeeks - A Computer Science Portal for Geeks"
+rem_vowel(string) 
+###############
+
+
+
