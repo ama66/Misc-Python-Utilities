6944c4a04d35068bfcdd2817954ee04c3aa330ea
Ammar Abdilghanie <ama66@cornell.edu>
2019-12-15 20:56:32 -0600

    Update Amazon-LeetCode.py

--- Amazon-LeetCode.py
+++ Amazon-LeetCode.py
@@ -601,3 +601,40 @@ class Solution:
             nums.insert(i, num)
             Accumulator.pop()
         return
+
+
+## https://leetcode.com/problems/merge-two-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        list_merged = current = ListNode(0)
+		
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val <= l2.val:
+                    current.next = ListNode(l1.val)
+                    current = current.next
+                    l1 = l1.next
+                else:
+                    current.next = ListNode(l2.val)
+                    current = current.next
+                    l2 = l2.next
+            else:
+                if not l1:
+                    current.next = ListNode(l2.val)
+                    current = current.next
+                    l2 = l2.next
+                else:
+                    current.next = ListNode(l1.val)
+                    current = current.next
+                    l1 = l1.next
+            
+        return list_merged.next
+
+
